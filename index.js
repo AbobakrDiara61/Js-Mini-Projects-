d2955d7ead570ff2eaddcf5601b75f50f5bce754
Muhamed Abobakr <muhammad.bakr2005@gmail.com>
2025-08-15 23:07:39 +0300

    Added Fetch Code Instead

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 // import createQuote from "./quotes.json";
 import * as Ui from "./ui.js";
 // import {createQuote} from "./quotes.json";
-const fetchApi = (api) => new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-    request.open("GET", api);
-    request.send();
-    request.onload = () => {
-      if(request.status === 200) resolve(JSON.parse(request.responseText)); // Getting Json Object
-      else reject(Error("File Not Found"));
-    }
-})
 // Getting Random Index
 function* getIndex(limit) {
   while(true) {
@@ -19,21 +10,11 @@ function* getIndex(limit) {
 // Getting Btn 
 const genBtn = document.querySelector(".generate-btn");
 genBtn.addEventListener("click", () => {
-  fetchApi("./quotes.json")
-  .then((jsonObj) => {
-    const randomIndex = getIndex(jsonObj.length);
-    document.body.appendChild(Ui.createQuoteElement(jsonObj[randomIndex.next().value]));
+  fetch("./quotes.json").then(result => result.json())
+  .then(jsonObj => {
+      const randomIndex = getIndex(jsonObj.length);
+      document.body.appendChild(Ui.createQuoteElement(jsonObj[randomIndex.next().value]))
   })
   .catch((error) => console.log(error));
 })
 genBtn.click(); 
-/* fetchApi("./quotes.json")
-.then((jsonObj) => {
-  const randomIndex = getIndex(jsonObj.length);
-  document.body.appendChild(Ui.createQuoteElement(jsonObj[randomIndex.next().value]));
-  genBtn.addEventListener("click", () => {
-    document.body.appendChild(Ui.createQuoteElement(jsonObj[randomIndex.next().value]));
-  })
-})
-.catch((error) => console.log(error));
-*/
