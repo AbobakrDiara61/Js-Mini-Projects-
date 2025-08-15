af2c98bb6108d9114ff98c8991fddccb782a29a3
Muhamed Abobakr <muhammad.bakr2005@gmail.com>
2025-08-15 23:02:09 +0300

    Improved The Code With Promises Next Version With Fetch API

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // import createQuote from "./quotes.json";
 import * as Ui from "./ui.js";
 // import {createQuote} from "./quotes.json";
-// Getting Json Object
-const request = new XMLHttpRequest();
-request.open("GET", "quotes.json");
-request.send();
+const fetchApi = (api) => new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open("GET", api);
+    request.send();
+    request.onload = () => {
+      if(request.status === 200) resolve(JSON.parse(request.responseText)); // Getting Json Object
+      else reject(Error("File Not Found"));
+    }
+})
 // Getting Random Index
 function* getIndex(limit) {
   while(true) {
@@ -13,18 +18,22 @@ function* getIndex(limit) {
 }
 // Getting Btn 
 const genBtn = document.querySelector(".generate-btn");
-request.onreadystatechange = () => {
-  if(request.status === 200 && request.readyState === 4) {
-    // const quotesJson = JSON.parse(request.responseText);
-    const quoteArray = [...JSON.parse(request.responseText)];
-    // console.log(quotesJson);
-    // console.log(quoteArray);
-    const randomIndex = getIndex(quoteArray.length);
-    // console.log(quoteArray[randomIndex.next().value])
-    document.body.appendChild(Ui.createQuoteElement(quoteArray[randomIndex.next().value]));
-    genBtn.addEventListener("click", () => {
-      document.body.appendChild(Ui.createQuoteElement(quoteArray[randomIndex.next().value]));
-    })
-    // document.body.appendChild(quoteArray[randomIndex.next().value]); 
-  }
-}
+genBtn.addEventListener("click", () => {
+  fetchApi("./quotes.json")
+  .then((jsonObj) => {
+    const randomIndex = getIndex(jsonObj.length);
+    document.body.appendChild(Ui.createQuoteElement(jsonObj[randomIndex.next().value]));
+  })
+  .catch((error) => console.log(error));
+})
+genBtn.click(); 
+/* fetchApi("./quotes.json")
+.then((jsonObj) => {
+  const randomIndex = getIndex(jsonObj.length);
+  document.body.appendChild(Ui.createQuoteElement(jsonObj[randomIndex.next().value]));
+  genBtn.addEventListener("click", () => {
+    document.body.appendChild(Ui.createQuoteElement(jsonObj[randomIndex.next().value]));
+  })
+})
+.catch((error) => console.log(error));
+*/
